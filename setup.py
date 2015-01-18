6216696ef94f83daf94671661140a40879a21ec1
Edward L Platt <ed@elplatt.com>
2015-01-18 01:13:05 -0500

    First commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='CivicPy',
+      version='1',
+      description='Extensions for numpy and scipy',
+      author='Ed Platt',
+      author_email='elplatt@mit.edu',
+      url='https://github.com/c4fcm/civicpy/',
+      packages=['civicpy', 'civicpy.stats'],
+     )
